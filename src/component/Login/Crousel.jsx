75f7d264292f93289c99abf56dc411030d5dfb37
Navneet kumar <navneetcool20@gmail.com>
2023-09-11 20:29:45 +0530

    crousel part

--- src/component/Login/Crousel.jsx
+++ src/component/Login/Crousel.jsx
@@ -1,29 +1,27 @@
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function Crousel() {
   return (
-    <div
-    style={{
-      height: "420px",
-        backgroundImage: "url('phone.png')",
-        backgroundSize: "contain",
-        backgroundRepeat: "no-repeat",
-        position: 'relative',
-
-    }}
-    >
-    
-      <img src="ss1.png"
-      height="300px"
-      alt="" 
-      style={{
-        position: 'absolute',
-          zIndex: 2,
-        left: "102px",
-        top: "17px",
-        height: "366px"
-      }}
-      />
+    <div className="crousel_container">
+     <Carousel>
+                <div className="crousel_wrapper">
+                    <img src="ss1.png" className="crousel_image" />
+                    
+                </div>
+                <div className="crousel_wrapper">
+                    <img src="ss2.png" className="crousel_image"/>
+                    
+                </div>
+                <div className="crousel_wrapper">
+                    <img src="ss3.png" className="crousel_image"/>
+                    
+                </div>
+                <div className="crousel_wrapper">
+                    <img src="ss4.png" className="crousel_image"/>
+                    
+                </div>
+            </Carousel>
     </div>
   )
 }
