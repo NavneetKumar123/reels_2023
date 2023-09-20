efd668b720fe37ac57a75f15e8d94b37a523f8c2
Navneet kumar <navneetcool20@gmail.com>
2023-09-20 20:22:35 +0530

    crousel complete

--- src/component/Login/Crousel.jsx
+++ src/component/Login/Crousel.jsx
@@ -3,27 +3,38 @@ import { Carousel } from 'react-responsive-carousel';
 
 function Crousel() {
   return (
-    <div className="crousel_container">
-     <Carousel>
-                <div className="crousel_wrapper">
-                    <img src="ss1.png" className="crousel_image" />
-                    
-                </div>
-                <div className="crousel_wrapper">
-                    <img src="ss2.png" className="crousel_image"/>
-                    
-                </div>
-                <div className="crousel_wrapper">
-                    <img src="ss3.png" className="crousel_image"/>
-                    
-                </div>
-                <div className="crousel_wrapper">
-                    <img src="ss4.png" className="crousel_image"/>
-                    
-                </div>
-            </Carousel>
+    <div
+      className="crousel_container"
+    >
+      <Carousel 
+      autoPlay={true}
+      showArrows={false}
+      showThumbs={false}
+      showStatus={false}
+        className="crousel_wrapper"
+        useKeyboardArrows={false}
+        showIndicators={false}
+        animationHandler="fade"  
+        swipeable={false} 
+        interval={1500}
+        transitionTime={500} 
+          >
+        <div>
+          <img src="ss1.png" className="crousel_image"/>
+        </div>
+        <div >
+          <img src="ss2.png" className="crousel_image" />
+        </div>
+        <div >
+          <img src="ss3.png" className="crousel_image" />
+        </div>
+        <div >
+          <img src="ss4.png" className="crousel_image" />
+        </div>
+      </Carousel>
     </div>
   )
 }
+
 
 export default Crousel
